ff752e5705fad7b599b6df72c45937279bf68532
Mith00 <18521396@gm.uit.edu.vn>
2019-12-22 14:53:32 +0700

    Create EXPLOSIVE.cs

---
+++ Sources/Gameplay/EXPLOSIVE.cs
@@ -0,0 +1,35 @@
+﻿#region Includes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Design;
+#endregion
+
+namespace PROJECT_SpaceShooter
+{
+    public class EXPLOSIVE : Basic2d
+    {
+        public EXPLOSIVE(Vector2 pos) : base("Explosive", pos, new Vector2(30, 30))
+        {
+        }
+
+        public override void Update(Vector2 OFFSET)
+        {
+            base.Update(OFFSET);
+        }
+
+        public override void Draw(Vector2 OFFSET)
+        {
+            base.Draw(OFFSET);
+        }
+    }
+}
